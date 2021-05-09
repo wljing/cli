f31f9b81fec5e2d052d2c831f1f3449bb61322d4
wljing <gdyr0902>
2021-05-09 19:27:47 +0800

    '下载模板文件完善'

--- commands/template.js
+++ commands/template.js
@@ -1,25 +1,37 @@
 const { resolve, join } = require('path');
-const { getRadio, readDir } = require('../utils');
+const { getRadio, readDir, getInput } = require('../utils');
 const download = require('download');
+const adm_zip = require('adm-zip');
+const { writeFileSync, fstat, rmdirSync, unlinkSync } = require('fs');
 
 const templateList = ['react'];
 
-const host = 'https://github.com/wljing/cli/tree/master/template/';
+const host = 'https://github.com/wljing/cli/raw/master/template/';
 
 module.exports = {
   descript: '模板',
-  exec: async ({ params }) => {
+  exec: async ({ params = [] }) => {
     let templateName = '';
-    if (params.length !== 0) {
-      if (Reflect.has(templateMap, params[0])) {
-        templateName = params[0];
-      } else {
-        console.log(`模板${params[0]} 不存在!\n模板列表:\n`);
-      }
+    let appName = params[1] || '';
+    if (params[0] && templateList.includes(params[0])) {
+      templateName = params[0];
+    }
+    while (appName === '') {
+      appName = await getInput('项目名称');
     }
     if (templateName === '') {
       templateName = await getRadio('项目模板', templateList);
-      const url = `${host}${templateName}.zip`;
     }
+    const execPath = process.cwd();
+    const url = `${host}${templateName}.zip`;
+    const filename = `${appName}.zip`;
+    await download(url, execPath, {
+      filename,
+    });
+    const tempFilePath = join(execPath, filename);
+    console.log('下载完成, 开始解压...', tempFilePath);
+    const zip = new adm_zip(tempFilePath);
+    zip.extractAllTo(execPath);
+    unlinkSync(tempFilePath);
   }
 }
