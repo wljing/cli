1266a21e6ff8f1f03d1e92b3acb9e6e57bd4375b
wljing <gdyr0902>
2021-05-09 19:28:36 +0800

    '下载模板文件完善'

--- react/src/route.jsx
+++
@@ -1,9 +0,0 @@
-import { HashRouter as Router, Route } from 'react-route-dom';
-
-const Home = () => <div>home page</div>;
-
-export default () => {
-  <Router>
-    <Route path="/" exact components={Home}></Route>
-  </Router>
-};
